68d6018b76a05a8038516b36f3f5d23c85a8e4cd
fishnchipa <wilsoncaox@gmail.com>
2024-02-09 20:57:22 +1100

    started working on saturation and value

--- components/ui/change-color/color-wheel.tsx
+++ components/ui/change-color/color-wheel.tsx
@@ -10,12 +10,13 @@ type ColorWheelProps = {
 
 const ColorWheel = ({size, radius, thickness}: ColorWheelProps) => {
 
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const wheelRef = useRef<HTMLCanvasElement>(null);
+  const svSquareRef = useRef<HTMLCanvasElement>(null);
   const hueRef = useRef<HTMLDivElement>(null);
   const [pressed, setPressed] = useState(false);
 
 
-  const draw = (ctx: CanvasRenderingContext2D) => {
+  const drawWheel = (ctx: CanvasRenderingContext2D) => {
     for (var i = 0; i < 360; i++) {
       ctx.beginPath();
       ctx.lineWidth = thickness;
@@ -25,14 +26,69 @@ const ColorWheel = ({size, radius, thickness}: ColorWheelProps) => {
     }
   }
 
-  
+  const drawSquare = (ctx: CanvasRenderingContext2D) => {
+    const square = svSquareRef.current;
+
+    if (square) {
+      const length = square.width;
+      const valueGradient = ctx.createLinearGradient(0, 0, 0, length);
+      const saturationGradient = ctx.createLinearGradient(0, 0, length, 0);
+      valueGradient.addColorStop(0, 'white');
+      valueGradient.addColorStop(1, 'black');
+      saturationGradient.addColorStop(0, 'hsla(27,100%,50%,0)');
+      saturationGradient.addColorStop(1, 'hsla(27,100%,50%,1)');
+      ctx.fillStyle = valueGradient;
+      ctx.fillRect(0, 0, length, length);
+      ctx.globalCompositeOperation = "multiply";
+      ctx.fillStyle = saturationGradient;
+      ctx.fillRect(0, 0, length, length);
+      ctx.globalCompositeOperation = "source-over";
+    }
+
+  }
+
+  const hsvToRGB = (value: number, saturation: number, hue: number) => {
+    let chroma = value * saturation;
+    let hue1 = hue / 60;
+    let x = chroma * (1- Math.abs((hue1 % 2) - 1));
+    let r1, g1, b1;
+    if (hue1 >= 0 && hue1 <= 1) {
+      ([r1, g1, b1] = [chroma, x, 0]);
+    } else if (hue1 >= 1 && hue1 <= 2) {
+      ([r1, g1, b1] = [x, chroma, 0]);
+    } else if (hue1 >= 2 && hue1 <= 3) {
+      ([r1, g1, b1] = [0, chroma, x]);
+    } else if (hue1 >= 3 && hue1 <= 4) {
+      ([r1, g1, b1] = [0, x, chroma]);
+    } else if (hue1 >= 4 && hue1 <= 5) {
+      ([r1, g1, b1] = [x, 0, chroma]);
+    } else if (hue1 >= 5 && hue1 <= 6) {
+      ([r1, g1, b1] = [chroma, 0, x]);
+    } else {
+      ([r1, g1, b1] = [0, 0, 0]);
+    }
+    
+    let m = value - chroma;
+    let [r,g,b] = [r1+m, g1+m, b1+m];
+    
+    // Change r,g,b values from [0,1] to [0,255]
+    return [255*r,255*g,255*b];
+  }
+
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      const ctx = canvas.getContext("2d");
-      if (ctx) {
-        ctx.imageSmoothingEnabled = false;
-        draw(ctx);
+    const wheel = wheelRef.current;
+    const square = svSquareRef.current;
+    if (wheel && square) {
+      const wheelCtx = wheel.getContext("2d");
+      const squareCtx = square.getContext("2d");
+      if (wheelCtx) {
+        wheelCtx.imageSmoothingEnabled = false;
+        drawWheel(wheelCtx);
+      }
+
+      if (squareCtx) {
+        squareCtx.imageSmoothingEnabled = false;
+        drawSquare(squareCtx);
       }
     }
   }, [])
@@ -41,6 +97,11 @@ const ColorWheel = ({size, radius, thickness}: ColorWheelProps) => {
     e.preventDefault();
     setPressed(true);
     
+  }
+
+  const onDragEnd = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    e.preventDefault();
+    setPressed(false)
   }
 
   const onDragMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
@@ -71,27 +132,22 @@ const ColorWheel = ({size, radius, thickness}: ColorWheelProps) => {
         y = -Math.sqrt(radius*radius - (x-a)*(x-a)) + b;
       }
 
-
       const hue = hueRef.current;
-      
       if (hue) {
         hue.style.left = (x) + "px";
         hue.style.top = (y) + "px";
       }
-
-   
     }
-
   }
 
-  const onDragEnd = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-    setPressed(false)
+  const updateHSV = () => {
+
   }
 
   return (
     <>
       <canvas 
-        ref={canvasRef} 
+        ref={wheelRef} 
         width={size}
         height={size} 
         className="bg-red-500"
@@ -103,9 +159,14 @@ const ColorWheel = ({size, radius, thickness}: ColorWheelProps) => {
         className="border-[2px] border-black ring-1 ring-offset-white absolute rounded-full pointer-events-none bg-transparent"
         ref={hueRef}
         style={{width: thickness, height: thickness}}
-      >
+      ></div>
+      <canvas 
+        ref={svSquareRef}
+        width={200}
+        height={200}
+        className="absolute"
+      />
 
-      </div>
     </>
   )
 }
