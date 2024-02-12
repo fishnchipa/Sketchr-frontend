dc09e349092b85490175756b25e454d54d60b08e
fishnchipa <wilsoncaox@gmail.com>
2024-02-12 23:48:30 +1100

    added global state using redux toolkit for local color

--- components/ui/change-color/color-display.tsx
+++ components/ui/change-color/color-display.tsx
@@ -2,20 +2,69 @@
 
 import React, { useState } from 'react'
 import ColorPrev from './color-prev';
+import { useAppSelector } from '@/lib/hooks';
 
-type ColorModelType = "RGB" | "HSV" | "HSL";
+type ColorModelType = "rgb" | "hsv" | "hsl";
 
-const colors = {
-	RGB: ["Red", "Green", "Blue"],
-	HSV: ["Hue", "Saturation", "Value"],
-	HSL: ["Hue", "Saturation", "Lightness"]
+type ColorObject = {
+	[key: string]: {
+		[key: string]: number;
+	};
 }
 
 
 
 const ColorDisplay = () => {
+	const globalColor: ColorObject = useAppSelector((state) => state.localColor);
+	const [colorModel, setColorModel] = useState<ColorModelType>("rgb");
 
-	const [colorModel, setColorModel] = useState<ColorModelType>("RGB");
+	const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+	}
+
+	const displayInput = (value: string) => {
+		if (colorModel == "hsl" || colorModel == "hsv") {
+			let displayValue = (globalColor[colorModel][value]);
+			if (value == "hue") {
+				displayValue = parseFloat((displayValue).toFixed(1));
+			} else {
+				displayValue = parseFloat((displayValue*100).toFixed(1));
+			}
+		
+			return (
+				<div key={value} className="flex flex-row justify-between items-center w-[200px]"> 
+					<h1 className="text-white text-[15px] font-semibold">{value}</h1>
+					<input 
+						className="w-[98px] h-[22px] bg-white ring-offset-background focus-visible:outline-none 
+						focus-visible:ring-0 focus-visible:ring-ring p-1 text-[10px]
+						placeholder-black focus-visible:placeholder-gray-500 font-semibold"
+						placeholder={displayValue.toString()}
+						type="number"
+						min="0"
+						max="100"
+						step=".1"
+					/>
+				</div>
+			)
+		} else {
+			return (
+				<div key={value} className="flex flex-row justify-between items-center w-[200px]"> 
+					<h1 className="text-white text-[15px] font-semibold">{value}</h1>
+					<input 
+						className="w-[98px] h-[22px] bg-white ring-offset-background focus-visible:outline-none 
+						focus-visible:ring-0 focus-visible:ring-ring p-1 text-[10px]
+						placeholder-black focus-visible:placeholder-gray-500 font-semibold"
+						placeholder={Math.round(globalColor[colorModel][value]).toString()}
+						type="number"
+						min="0"
+						max="255"
+						step=".1"
+						onChange={(e) => {}}
+					/>
+				</div>
+			)
+		}
+	}
 
 	return (
 		<div className="w-[265px] h-[175px] rounded-[20px] bg-[#9B9B9B] flex flex-col items-center py-[10px] gap-y-[18px]">
@@ -25,35 +74,29 @@ const ColorDisplay = () => {
 			>
 				<div 
 					className={`w-[72px] h-[18px] rounded-[20px] bg-[#7B7B7B] absolute
-					${(colorModel === "HSV") ? "left-[75.5px]" : 
-					((colorModel === "HSL") ? "left-[149px] " : "left-[1.5px]")} transition-all`}
+					${(colorModel === "hsv") ? "left-[75.5px]" : 
+					((colorModel === "hsl") ? "left-[149px] " : "left-[1.5px]")} transition-all`}
 				
 				></div>
 				<div className="w-[72px] h-[18px] rounded-[20px] flex justify-center items-center z-10">
-					<button onClick={() => {setColorModel("RGB")}}>
+					<button onClick={() => {setColorModel("rgb")}}>
 						<h1 className="text-[10px] text-white font-semibold">RGB</h1>
 					</button>
 				</div>
 				<div className="w-[72px] h-[18px] rounded-[20px] flex justify-center items-center z-10">
-					<button onClick={() => {setColorModel("HSV")}}>
+					<button onClick={() => {setColorModel("hsv")}}>
 						<h1 className="text-[10px] text-white font-semibold">HSV</h1>
 					</button>
 				</div>
 				<div className="w-[72px] h-[18px] rounded-[20px] flex justify-center items-center z-10">
-					<button onClick={() => {setColorModel("HSL")}}>
+					<button onClick={() => {setColorModel("hsl")}}>
 						<h1 className="text-[10px] text-white font-semibold">HSL</h1>
 					</button>
 				</div>
 			</div>
-			<div className="flex flex-col justify-center gap-y-[11px]">
-				{colors[colorModel].map((value) => {
-					return (
-						<div key={value} className="flex flex-row justify-between items-center w-[163px]"> 
-							<h1 className="text-white text-[10px] font-semibold tracking-wider">{value}</h1>
-							<input className="w-[98px] h-[22px] bg-white ring-offset-background focus-visible:outline-none 
-							focus-visible:ring-0 focus-visible:ring-ring p-1 text-[10px] "/>
-						</div>
-					)
+			<div className="flex flex-col justify-center gap-y-[5px]">
+				{Object.keys(globalColor[colorModel]).map((value) => {
+					return displayInput(value);
 				})}
 			</div>
 			<ColorPrev />
