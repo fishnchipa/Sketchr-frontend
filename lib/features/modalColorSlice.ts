ef6bf89a5dea2391ca8646a999c1837ccbf6928c
fishnchipa <wilsoncaox@gmail.com>
2024-02-12 23:52:48 +1100

    state for open and close

--- lib/features/modalColorSlice.ts
+++ lib/features/modalColorSlice.ts
@@ -3,13 +3,13 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 const initialState = { open: false };
 
 const modalColorSlice = createSlice({
-	name: "modalColor",
-	initialState,
-	reducers: {
-		openModalColor: (state, action: PayloadAction<boolean>) => {
-			state.open = action.payload;
-		}
-	}
+  name: "modalColor",
+  initialState,
+  reducers: {
+    openModalColor: (state, action: PayloadAction<boolean>) => {
+      state.open = action.payload;
+    }
+  }
 })
 
 export const { openModalColor } = modalColorSlice.actions;
