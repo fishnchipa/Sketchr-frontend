d5dd4fde3c467771242ccbb69da95798037b396c
fishnchipa <wilsoncaox@gmail.com>
2024-02-12 23:52:09 +1100

    global access to color

--- lib/features/globalColorSlice.ts
+++ lib/features/globalColorSlice.ts
@@ -2,33 +2,33 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { ColorType } from "../types";
 
 const initialState = {
-	rgb: {
-		red: 128,
-		green: 64,
-		blue: 64 
-	}, 
-	hsv: {
-		hue: 0,
-		saturation: 0.5,
-		value: 0.5
-	},
-	hsl: {
-		hue: 0,
-		saturation: 0.333,
-		lightness: 0.375
-	}
+  rgb: {
+    red: 128,
+    green: 64,
+    blue: 64 
+  }, 
+  hsv: {
+    hue: 0,
+    saturation: 0.5,
+    value: 0.5
+  },
+  hsl: {
+    hue: 0,
+    saturation: 0.333,
+    lightness: 0.375
+  }
 }
 
 const globalColorSlice = createSlice({
-	name: "globalColor",
-	initialState,
-	reducers: {
-		setGlobalColor: (state, action: PayloadAction<ColorType>) => {
-			state.hsl = action.payload.hsl;
-			state.hsv = action.payload.hsv;
-			state.rgb = action.payload.rgb; 
-		}
-	}
+  name: "globalColor",
+  initialState,
+  reducers: {
+    setGlobalColor: (state, action: PayloadAction<ColorType>) => {
+      state.hsl = action.payload.hsl;
+      state.hsv = action.payload.hsv;
+      state.rgb = action.payload.rgb; 
+    }
+  }
 
 })
 
