a7e50aaab58768176ebc5bf9c052bc58dc27a804
fishnchipa <wilsoncaox@gmail.com>
2024-02-13 20:41:59 +1100

    started on header

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      
+      <div 
+        className="h-[56px] bg-[#494949] stroke-black flex-row items-center 
+        drop-shadow-[0_1px_2px_rgba(0,0,0,1.25)]">
+        
+      </div>
     </header>
   )
 }
