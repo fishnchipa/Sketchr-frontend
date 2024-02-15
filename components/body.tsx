5b4813597127a50fdb3d5b74f9f98479a4e4d869
wilson <z5476624@ad.unsw.edu.au>
2024-02-15 22:53:58 +1100

    started on body

---
+++ components/body.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Body = () => {
+	return (
+		<div className="h-full w-full bg-[#868686] flex justify-center items-center">
+			<div className="h-[500px] w-[1000px] bg-white">
+
+			</div>
+		</div>
+
+		
+	)
+}
+
+export default Body
