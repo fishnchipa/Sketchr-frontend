5b32eff9f31fe5088021e351d90d7ff7adf2215c
wilson <z5476624@ad.unsw.edu.au>
2024-02-15 22:53:25 +1100

    finished design of header

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
+import UiButton from './ui/ui-button'
+import SliderButton from './ui/slider-button'
 
 const Header = () => {
   return (
     <header>
       <div 
-        className="h-[56px] bg-[#494949] stroke-black flex-row items-center 
-        drop-shadow-[0_1px_2px_rgba(0,0,0,1.25)]">
-        
+        className="h-[56px] w-full bg-[#494949] stroke-black flex flex-row
+        drop-shadow-[0_1px_2px_rgba(0,0,0,1.25)] border-b-[2px] border-black
+        "
+      >
+        <div 
+          className="h-[56px] flex justify-center items-center min-w-[56px]
+          border-black border-r-[2px]"
+          >
+            <UiButton icon={'/home.png'} fn={() => {}}/>
+        </div>
+        <div className="flex flex-row justify-between items-center w-full px-[30px]">
+          <div className="flex flex-row gap-x-[15px]">
+            <UiButton icon={'/color.png'} fn={() => {}}/>
+            <SliderButton text={"Size"} increment={(num: number) => {console.log("Size: " + num)}}/>
+            <SliderButton text={"Opacity"} increment={(num: number) => {console.log("Opacity: " + num)}}/>
+          </div>
+          <div className="flex flex-row gap-x-[15px]">
+            <UiButton icon={'/people.png'} fn={() => {}}/>
+            <UiButton icon={'/layer.png'} fn={() => {}}/>
+          </div>
+        </div>
+          
       </div>
-    </header>
+    </header> 
   )
 }
 
