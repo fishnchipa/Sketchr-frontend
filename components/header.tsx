1491de3f48bbe79bf467cde96bdbcd1335da5b44
wilson <z5476624@ad.unsw.edu.au>
2024-02-16 22:28:12 +1100

    changed ui button instance to designated function

--- components/header.tsx
+++ components/header.tsx
@@ -1,30 +1,34 @@
 import React from 'react'
-import UiButton from './ui/ui-button'
-import SliderButton from './ui/slider-button'
+
+import SliderButton from '@/components/ui/slider-button'
+import HomeButton from '@/components/ui/fn-button/home-button'
+import ColorButton from '@/components/ui/fn-button/color-button'
+import UsersButton from '@/components/ui/fn-button/users-button'
+import LayersButton from '@/components/ui/fn-button/layers-button'
 
 const Header = () => {
   return (
     <header>
       <div 
         className="h-[56px] w-full bg-[#494949] stroke-black flex flex-row
-        drop-shadow-[0_1px_2px_rgba(0,0,0,1.25)] border-b-[2px] border-black
+        drop-shadow-[0_1px_2px_rgba(0,0,0,0.5)] border-b-[2px] border-black
         "
       >
         <div 
           className="h-[56px] flex justify-center items-center min-w-[56px]
           border-black border-r-[2px]"
           >
-            <UiButton icon={'/home.png'} fn={() => {}}/>
+            <HomeButton />
         </div>
         <div className="flex flex-row justify-between items-center w-full px-[30px]">
           <div className="flex flex-row gap-x-[15px]">
-            <UiButton icon={'/color.png'} fn={() => {}}/>
+            <ColorButton />
             <SliderButton text={"Size"} increment={(num: number) => {console.log("Size: " + num)}}/>
             <SliderButton text={"Opacity"} increment={(num: number) => {console.log("Opacity: " + num)}}/>
           </div>
           <div className="flex flex-row gap-x-[15px]">
-            <UiButton icon={'/people.png'} fn={() => {}}/>
-            <UiButton icon={'/layer.png'} fn={() => {}}/>
+            <UsersButton />
+            <LayersButton />
           </div>
         </div>
           
