42637360c99b8e72a23afbedba4240cd23674bc2
wilson <z5476624@ad.unsw.edu.au>
2024-02-16 22:28:39 +1100

    added button displays

--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import UiButton from './ui/ui-button'
-import Separator from './ui/separator'
+import { HSeparator } from './ui/separator'
 
 
 const SideBar = () => {
 	return (
 		<div className="w-[56px] h-full bg-[#494949] flex flex-col items-center gap-y-[15px] pt-[35px] border-r-[2px] border-black">
-			<UiButton icon={'/brush.png'} fn={() => {}} />
-			<UiButton icon={'/eraser.png'} fn={() => {}} />
-			<UiButton icon={'/cursor.png'} fn={() => {}} />
-			<UiButton icon={'/fill.png'} fn={() => {}} />
-			<UiButton icon={'/eyedrop.png'} fn={() => {}} />
-			<Separator />
-			<UiButton icon={'/transform.png'} fn={() => {}} />
-			<UiButton icon={'/move.png'} fn={() => {}} />
-			<Separator />
-			<UiButton icon={'/selection.png'} fn={() => {}} />
-			<UiButton icon={'/circleSelection.png'} fn={() => {}} />
+			<UiButton icon={'/brush.png'} fn={() => { } } isSelected={false} />
+			<UiButton icon={'/eraser.png'} fn={() => { } } isSelected={false} />
+			<UiButton icon={'/cursor.png'} fn={() => { } } isSelected={false} />
+			<UiButton icon={'/fill.png'} fn={() => { } } isSelected={false} />
+			<UiButton icon={'/eyedrop.png'} fn={() => { } } isSelected={false} />
+			<HSeparator />
+			<UiButton icon={'/transform.png'} fn={() => { } } isSelected={false} />
+			<UiButton icon={'/move.png'} fn={() => { } } isSelected={false} />
+			<HSeparator />
+			<UiButton icon={'/selection.png'} fn={() => { } } isSelected={false} />
+			<UiButton icon={'/circleSelection.png'} fn={() => { } } isSelected={false} />
 		</div>
 	)
 }
