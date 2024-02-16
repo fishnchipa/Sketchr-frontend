8719991fef31bdf003933326bbac94c6199336eb
wilson <z5476624@ad.unsw.edu.au>
2024-02-16 22:29:25 +1100

    added vertical and horiztonal separators

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Separator = () => {
+export const HSeparator = () => {
 	return (
 		<hr className="h-[1px] w-[45px] bg-[#878787]"/>
 	)
 }
 
-export default Separator
+export const VSeparator = () => {
+	return (
+		<hr className="h-[40px] w-[1px] bg-[#878787]" />
+	)
+}
+
