ab532532184cf98d7fba93a21bd6c8be4071c35b
wilson <z5476624@ad.unsw.edu.au>
2024-02-16 22:26:41 +1100

    initial implemenetation of footer

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<div className="h-[40px] w-full bg-[#494949] fixed bottom-0 z-0">
+			<div className="w-[56px] border-r-[2px] border-black h-full">
+
+			</div>
+		</div>
+	)
+}
+
+export default Footer
