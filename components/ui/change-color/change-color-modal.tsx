93802defda6e1de3c24c8c47d1a6abe6aabf7273
wilson <z5476624@ad.unsw.edu.au>
2024-02-16 22:29:07 +1100

    change bg opacity of modal

--- components/ui/change-color/change-color-modal.tsx
+++ components/ui/change-color/change-color-modal.tsx
@@ -7,11 +7,12 @@ import ColorHex from './color-hex'
 import { useAppDispatch, useAppSelector } from '@/lib/hooks'
 import { setGlobalColor } from '@/lib/features/globalColorSlice'
 import { resetColor } from '@/lib/features/localColorSlice'
-import { openModalColor } from '@/lib/features/modalColorSlice'
+import { closeModalColor } from '@/lib/features/modalColorSlice'
 
 const ChangeColorModal = () => {
   const localColor = useAppSelector((state) => state.localColor);
   const globalColor = useAppSelector((state) => state.globalColor);
+  const modal = useAppSelector((state) => state.modalColor);
   const dispatch = useAppDispatch();
   
 
@@ -20,54 +21,57 @@ const ChangeColorModal = () => {
   const changeColor = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     dispatch(setGlobalColor(localColor));
-    dispatch(openModalColor(false))
+    dispatch(closeModalColor())
     
   }
 
   const cancel = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     dispatch(resetColor(globalColor));
-    dispatch(openModalColor(false));
+    dispatch(closeModalColor());
    
   }
-
-  return (
-    <>
-      <div
-        className="w-[500px] h-[600px] rounded-[20px] bg-[#494949] p-[25px] flex flex-col gap-y-[25px]"
-      >
-        <div className="w-[450px] h-[350px] flex justify-center items-center bg-[#9B9B9B] rounded-[20px]">
-          <ColorWheel 
-            size={325} 
-            radius={149} 
-            thickness={26} 
-          />
-        </div>
-        <div className="flex flex-row gap-x-[25px]">
-          <ColorDisplay />
-          <div className="flex flex-col gap-y-[25px]">
-            <ColorHex />
-            <div className="flex flex-row gap-x-[4px]">
-              <button 
-                onClick={(e) => {cancel(e)}}
-                className="flex justify-center items-center w-[78px] h-[41px] rounded-[20px] 
-                bg-white text-black text-[15px] font-semibold hover:bg-[#c2c2c2]"
-              >
-                Cancel
-              </button>
-              <button 
-                onClick={(e) => {changeColor(e)}}
-                className="flex justify-center items-center w-[78px] h-[41px] rounded-[20px]
-                bg-white text-black text-[15px] font-semibold hover:bg-[#c2c2c2]"
-                >
-                Ok
-              </button>
+  if (modal.open) {
+    return (
+      <>
+        <div className="w-screen h-screen fixed flex justify-center items-center bg-opacity-80 bg-black z-10">
+          <div
+            className="w-[500px] h-[600px] rounded-[20px] bg-[#6d6d6d] p-[25px] flex flex-col gap-y-[25px]"
+          >
+            <div className="w-[450px] h-[350px] flex justify-center items-center bg-[#9B9B9B] rounded-[20px]">
+              <ColorWheel 
+                size={325} 
+                radius={149} 
+                thickness={26} 
+              />
+            </div>
+            <div className="flex flex-row gap-x-[25px]">
+              <ColorDisplay />
+              <div className="flex flex-col gap-y-[25px]">
+                <ColorHex />
+                <div className="flex flex-row gap-x-[4px]">
+                  <button 
+                    onClick={(e) => {cancel(e)}}
+                    className="flex justify-center items-center w-[78px] h-[41px] rounded-[20px] 
+                    bg-white text-black text-[15px] font-semibold hover:bg-[#c2c2c2]"
+                  >
+                    Cancel
+                  </button>
+                  <button 
+                    onClick={(e) => {changeColor(e)}}
+                    className="flex justify-center items-center w-[78px] h-[41px] rounded-[20px]
+                    bg-white text-black text-[15px] font-semibold hover:bg-[#c2c2c2]"
+                    >
+                    Ok
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default ChangeColorModal
