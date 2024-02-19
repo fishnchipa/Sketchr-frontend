0a43d0b9bd698aad79602403de974a86a1de951f
fishnchipa <wilsoncaox@gmail.com>
2024-02-19 23:06:45 +1100

    change the state of the layer

--- components/ui/layers-menu/layer.tsx
+++ components/ui/layers-menu/layer.tsx
@@ -2,20 +2,35 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import { VSeparator } from '../separator';
 import CanvasLayer from './canvas-layer';
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { changeLayer } from '@/lib/features/layersMenuSlice';
 
 type LayerProps = {
+  id: number
   name: string,
+  isSelected: boolean
 }
 
-const Layer = ({name}: LayerProps) => {
+const Layer = ({id, name, isSelected}: LayerProps) => {
+
+  const dispatch = useAppDispatch();
   const [hidden, setHidden] = useState(false); 
 
   const changeHidden = () => {
     setHidden(prev => !prev);
   }
 
+  const switchLayer = () => {
+    dispatch(changeLayer(id));
+
+  }
+
   return (
-    <div className="w-full h-[50px] bg-[#444444] rounded-[5px] flex flex-row gap-x-[15px] items-center px-[13px]">
+    <div 
+      className={`w-full h-[50px] bg-[#444444] rounded-[5px] flex flex-row gap-x-[15px] items-center px-[13px] 
+      ${isSelected ? "border-dashed border-[#9c9c9c] border-[3px]" : "border-none"} `}
+      onClick={switchLayer}
+    >
       <button onClick={changeHidden}>
         {hidden ? 
         (<Image src={"/closed-eye.png"} alt={'hidden'} width={25} height={25}/>) : 
