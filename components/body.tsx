ef520ef47eab181653ff5ec9a260be5a6b25c52c
fishnchipa <wilsoncaox@gmail.com>
2024-02-20 21:39:48 +1100

    removed hidden prop

--- components/body.tsx
+++ components/body.tsx
@@ -4,26 +4,20 @@ import React, { useRef, useState } from 'react'
 import ChangeColorModal from './ui/change-color/change-color-modal'
 import Canvas from './ui/layers-menu/canvas'
 import { useAppSelector } from '@/lib/hooks'
-import { CanvasRef } from '@/lib/types'
 
 const Body = () => {
 	const canvas = useAppSelector((state) => state.layerMenu);
-	const layerRef = useRef<CanvasRef[]>([]);
-	const [attribute, setAttribute] = useState(true);
-	
 
 	return (
 		<>
 			<ChangeColorModal />
 			<div className="h-full w-full bg-[#868686] flex justify-center items-center">
-				
-				<div className="h-[500px] w-[1000px] bg-white">
+				<div className="h-[500px] w-[1000px] bg-white" >
 					{canvas.layers.map((value, index) => {
 						return (
-							<Canvas key={value} ref={(el) => layerRef.current[index] = el!} id={index}/>
+							<Canvas key={value} id={index}/>
 						)
 					})}
-					
 				</div>
 			</div>
 		</>
