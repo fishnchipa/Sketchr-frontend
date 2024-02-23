6742446f54076f2827ae0860c9a44373bd7ce8b8
fishnchipa <wilsoncaox@gmail.com>
2024-02-23 17:05:40 +1100

    changed indentation

--- components/ui/users-menu/users-menu.tsx
+++ components/ui/users-menu/users-menu.tsx
@@ -20,7 +20,7 @@ const UsersMenu = () => {
 
 	if (usersMenu.open) {
 		return (
-			<div className="w-[280px] h-full bg-[#494949] border-l-[2px] border-black flex flex-col items-center pt-[40px] gap-y-[32px]">
+			<div className="w-[280px] h-full bg-[#494949] border-l-[2px] border-black flex flex-col items-center pt-[40px] gap-y-[32px] px-5">
 				<div className="w-[242px] h-[45px] rounded-[60px] flex flex-row items-center justify-center relative bg-[#595959] gap-x-[5px]">
 					<div className={`w-[113px] h-[35px] rounded-[60px] absolute bg-[#9B9B9B]
 					${usersMenu.friendsRequests ? "left-[6px]" : "left-[124px]"} transition-all`}></div>
