0c523cf5a64113c8c22de17774db35a4a0bdd600
fishnchipa <wilsoncaox@gmail.com>
2024-02-28 23:50:31 +1100

    added draggable functionality to layer div

--- components/ui/layers-menu/layer.tsx
+++ components/ui/layers-menu/layer.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { VSeparator } from '../separator';
 import CanvasLayer from './canvas-layer';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
@@ -16,7 +16,9 @@ type LayerProps = {
 const Layer = ({id, name, isSelected}: LayerProps) => {
   const menu = useAppSelector((state) => state.layerMenu);
   const dispatch = useAppDispatch();
-  const [hidden, setHidden] = useState(false); 
+  const [hidden, setHidden] = useState(false);
+  const layerRef = useRef<HTMLDivElement>(null); 
+  const isDragging = useRef(false);
 
   const changeHidden = () => {
     setHidden(prev => !prev);
@@ -30,15 +32,46 @@ const Layer = ({id, name, isSelected}: LayerProps) => {
 
   const switchLayer = () => {
     dispatch(changeLayer(id));
+
+  }
+
+  const changePositionAbsolute = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!layerRef.current) {
+      return;
+    }
     
+    layerRef.current.style.position = "absolute";
+  }
+
+  const changePositionStatic = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!layerRef.current) {
+      return;
+    }
+
+    layerRef.current.style.position = "static";
+    layerRef.current.style.left = "0px"
+  }
+
+  const moveDraggable = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!layerRef.current) {
+      return;
+    }
+    const bound = e.currentTarget.getBoundingClientRect();
+    console.log(e.clientY - bound.y + e.currentTarget.offsetTop - 25);
+    layerRef.current.style.left = (e.clientX - bound.x + e.currentTarget.offsetLeft - (e.currentTarget.offsetWidth/2)) + "px";
+    layerRef.current.style.top = (e.clientY - bound.y + e.currentTarget.offsetTop - e.currentTarget.offsetHeight/2) + "px";
   }
 
   return (
     <div 
       className={`w-full h-[50px] bg-[#444444] rounded-[5px] flex flex-row gap-x-[15px] items-center px-[13px] 
       ${isSelected ? "border-dashed border-[#9c9c9c] border-[3px]" : "border-none"} `}
+      ref={layerRef}
       onClick={switchLayer}
- 
+      onMouseDown={(e) => changePositionAbsolute(e)}
+      onMouseUp={(e) => changePositionStatic(e)}
+      onMouseMove={(e) => {moveDraggable(e)}}
+
     >
       <button onClick={changeHidden}>
         {hidden ? 
