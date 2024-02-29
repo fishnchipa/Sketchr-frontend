acc6f925838b62f8686bb9a09de21732e2cdd2a1
fishnchipa <wilsoncaox@gmail.com>
2024-02-29 18:19:04 +1100

    fixed mouseUp so that it takes the index instead of the id

--- components/body.tsx
+++ components/body.tsx
@@ -9,18 +9,25 @@ import { CanvasRef } from '@/lib/types'
 const Body = () => {
 	const canvas = useAppSelector((state) => state.layerMenu);
 	const canvasRefs = useRef<CanvasRef[] >([])
+	
+	const mouseUp = () =>{
+		const index = canvas.layers.findIndex(value => value.id === canvas.selected);
+		canvasRefs.current[index].mouseUp();
+	}
 
 	return (
 		<>
 			<ChangeColorModal />
 			<div className="h-full w-full bg-[#868686] flex justify-center items-center"
-				onMouseUp={() => canvasRefs.current[canvas.selected].mouseUp()}
+				onMouseUp={() => mouseUp()}
 			
 			>
 				<div className="relative h-[500px] w-[1000px] bg-white" >
-					{canvas.layers.map((value, index) => {
+					{canvas.layers.map((value) => {
+						const index = canvas.layers.findIndex(item => item.id === value.id);
+
 						return (
-							<Canvas key={value} id={index} ref={(el) => canvasRefs.current[index] = el!}/>
+							<Canvas key={value.id} id={value.id} ref={(el) => canvasRefs.current[index] = el!}/>
 							
 						)
 					})}
